ec91d48c50675c444e5b8cb5e6511197722aef42
GRUPOPM\opimentel <oliver.pimentel@people-media.com.mx>
2023-04-11 20:23:45 -0600

    Se modifica querys para tomar fechas no obligatorias, asi como el tipo de donador

--- src/main/java/com/imss/sivimss/donaciones/beans/ConsultaDonado.java
+++ src/main/java/com/imss/sivimss/donaciones/beans/ConsultaDonado.java
@@ -1,8 +1,6 @@
 package com.imss.sivimss.donaciones.beans;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.xml.bind.DatatypeConverter;
 
@@ -30,9 +28,9 @@ public class ConsultaDonado {
 	private String numInventario;
 	private Date fecDonacion;
 	private String donadoPor;
-	private String nomDonador ;
-	private Integer idVelatorio; 
-	private Integer idDelegacion; 
+	private String nomDonador;
+	private Integer idVelatorio;
+	private Integer idDelegacion;
 	private String fechaInicio;
 	private String fechaFin;
 
@@ -47,8 +45,8 @@ public class ConsultaDonado {
 		this.fecDonacion = consultaDonadoRequest.getFecDonacion();
 		this.donadoPor = consultaDonadoRequest.getDonadoPor();
 		this.nomDonador = consultaDonadoRequest.getNomDonador();
-		this.idVelatorio = consultaDonadoRequest.getIdVelatorio(); 
-		this.idDelegacion = consultaDonadoRequest.getIdDelegacion(); 
+		this.idVelatorio = consultaDonadoRequest.getIdVelatorio();
+		this.idDelegacion = consultaDonadoRequest.getIdDelegacion();
 		this.fechaInicio = consultaDonadoRequest.getFechaInicio();
 		this.fechaFin = consultaDonadoRequest.getFechaFin();
 		this.desDelegacion = consultaDonadoRequest.getDesDelegacion();
@@ -56,58 +54,61 @@ public class ConsultaDonado {
 
 	public DatosRequest consultaDonado(DatosRequest request, String formatoFecha) {
 
-        SelectQueryUtil queryUtil = new SelectQueryUtil();
-        queryUtil.select("si.ID_INVENTARIO AS idInventario", "sv.NOM_VELATORIO AS velatorio","stm.DES_TIPO_MATERIAL AS tipo"
-        		,"sa.DES_MODELO_ARTICULO AS modeloAtaud", "si.NUM_INVENTARIO AS numInventario","date_format(ssd.FEC_SOLICITUD,'"+ formatoFecha + "')  AS fecDonacion"
-        		,"ssd.NOMBRE_INSTITUCION AS donadoPor","sp.NOM_PERSONA AS nomDonador")
-        .from("svc_inventario si")
-        .innerJoin("svt_articulo sa", "sa.ID_ARTICULO = si.ID_ARTICULO")
-        .innerJoin("svc_velatorio sv", "sv.ID_VELATORIO = si.ID_VELATORIO")
-        .innerJoin("svc_tipo_material stm", "stm.ID_TIPO_MATERIAL = sa.ID_TIPO_MATERIAL")
-        .innerJoin("svc_salida_donacion_ataudes ssda","ssda.ID_ARTICULO = sa.ID_ARTICULO")
-		.innerJoin("svc_salida_donacion ssd","ssd.ID_SALIDA_DONACION  = ssda.ID_SALIDA_DONACION")
-		.innerJoin("svc_contratante sc","sc.ID_CONTRATANTE = ssd.ID_CONTRATANTE")
-		.innerJoin("svc_persona sp","sp.ID_PERSONA = sc.ID_PERSONA ");
-        final String query = queryUtil.build();
+		SelectQueryUtil queryUtil = new SelectQueryUtil();
+		queryUtil
+				.select("si.ID_INVENTARIO AS idInventario", "sv.NOM_VELATORIO AS velatorio",
+						"stm.DES_TIPO_MATERIAL AS tipo", "sa.DES_MODELO_ARTICULO AS modeloAtaud",
+						"si.NUM_INVENTARIO AS numInventario",
+						"date_format(ssd.FEC_SOLICITUD,'" + formatoFecha + "')  AS fecDonacion",
+						"ssd.NOMBRE_INSTITUCION AS donadoPor", "sp.NOM_PERSONA AS nomDonador")
+				.from("svc_inventario si").innerJoin("svt_articulo sa", "sa.ID_ARTICULO = si.ID_ARTICULO")
+				.innerJoin("svc_velatorio sv", "sv.ID_VELATORIO = si.ID_VELATORIO")
+				.innerJoin("svc_tipo_material stm", "stm.ID_TIPO_MATERIAL = sa.ID_TIPO_MATERIAL")
+				.innerJoin("svc_salida_donacion_ataudes ssda", "ssda.ID_ARTICULO = sa.ID_ARTICULO")
+				.innerJoin("svc_salida_donacion ssd", "ssd.ID_SALIDA_DONACION  = ssda.ID_SALIDA_DONACION")
+				.innerJoin("svc_contratante sc", "sc.ID_CONTRATANTE = ssd.ID_CONTRATANTE")
+				.innerJoin("svc_persona sp", "sp.ID_PERSONA = sc.ID_PERSONA ");
+		final String query = queryUtil.build();
 		String encoded = DatatypeConverter.printBase64Binary(query.getBytes());
 		request.getDatos().put(AppConstantes.QUERY, encoded);
 
 		return request;
 	}
 
-	public DatosRequest consultarFiltroDonados(DatosRequest request, String formatoFecha)  {
-        SelectQueryUtil queryUtil = new SelectQueryUtil();
-		queryUtil.select("si.ID_INVENTARIO AS idInventario"," sv.NOM_VELATORIO AS velatorio","stm.DES_TIPO_MATERIAL AS tipo"
-	    		," sa.DES_MODELO_ARTICULO AS modeloAtaud "," si.NUM_INVENTARIO AS numInventario "
-	    		," date_format(ssd.FEC_SOLICITUD,'" + formatoFecha + "')  AS fecDonacion"," ssd.NOMBRE_INSTITUCION AS donadoPor"," sp.NOM_PERSONA AS nomDonador")
-				.from("svc_inventario si ")
-				.innerJoin("svt_articulo sa","sa.ID_ARTICULO = si.ID_ARTICULO")
-				.innerJoin("svc_velatorio sv","sv.ID_VELATORIO = si.ID_VELATORIO")
-				.innerJoin("svc_tipo_material stm","stm.ID_TIPO_MATERIAL = sa.ID_TIPO_MATERIAL")
-				.innerJoin("svc_salida_donacion_ataudes ssda","ssda.ID_ARTICULO = sa.ID_ARTICULO")
-				.innerJoin("svc_salida_donacion ssd","ssd.ID_SALIDA_DONACION  = ssda.ID_SALIDA_DONACION")
-				.innerJoin("svc_contratante sc","sc.ID_CONTRATANTE = ssd.ID_CONTRATANTE")
-				.innerJoin("svc_persona sp","sp.ID_PERSONA = sc.ID_PERSONA")
-				.innerJoin("svc_delegacion sd","sd.ID_DELEGACION = sv.ID_DELEGACION")
-				.where("si.ID_VELATORIO = :idVel")
-				.setParameter("idVel", this.idVelatorio)
-				.and("sv.ID_DELEGACION = :idDel")
-				.setParameter("idDel", this.idDelegacion) 
-				.and("ssd.FEC_SOLICITUD >= :fecIni")
-				.setParameter("fecIni", this.fechaInicio)
-				.and("ssd.FEC_SOLICITUD <= :fecFin")
-				.setParameter("fecFin",this.fechaFin);
-        final String query = queryUtil.build();
+	public DatosRequest consultarFiltroDonadosSalida(DatosRequest request, String formatoFecha) {
+		SelectQueryUtil queryUtil = new SelectQueryUtil();
+		queryUtil
+				.select("si.ID_INVENTARIO AS idInventario", " sv.NOM_VELATORIO AS velatorio",
+						"stm.DES_TIPO_MATERIAL AS tipo", " sa.DES_MODELO_ARTICULO AS modeloAtaud ",
+						" si.NUM_INVENTARIO AS numInventario ",
+						" date_format(ssd.FEC_SOLICITUD,'" + formatoFecha + "')  AS fecDonacion",
+						" ssd.NOMBRE_INSTITUCION AS donadoPor", " sp.NOM_PERSONA AS nomDonador")
+				.from("svc_inventario si ").innerJoin("svt_articulo sa", "sa.ID_ARTICULO = si.ID_ARTICULO")
+				.innerJoin("svc_velatorio sv", "sv.ID_VELATORIO = si.ID_VELATORIO")
+				.innerJoin("svc_tipo_material stm", "stm.ID_TIPO_MATERIAL = sa.ID_TIPO_MATERIAL")
+				.innerJoin("svc_salida_donacion_ataudes ssda", "ssda.ID_ARTICULO = sa.ID_ARTICULO")
+				.innerJoin("svc_salida_donacion ssd", "ssd.ID_SALIDA_DONACION  = ssda.ID_SALIDA_DONACION")
+				.innerJoin("svc_contratante sc", "sc.ID_CONTRATANTE = ssd.ID_CONTRATANTE")
+				.innerJoin("svc_persona sp", "sp.ID_PERSONA = sc.ID_PERSONA")
+				.innerJoin("svc_delegacion sd", "sd.ID_DELEGACION = sv.ID_DELEGACION").where("si.ID_VELATORIO = :idVel")
+				.setParameter("idVel", this.idVelatorio).and("sv.ID_DELEGACION = :idDel")
+				.setParameter("idDel", this.idDelegacion);
+		if (this.fechaInicio != null && this.fechaFin != null) {
+			queryUtil.and("ssd.FEC_SOLICITUD >= :fecIni").setParameter("fecIni", this.fechaInicio)
+					.and("ssd.FEC_SOLICITUD <= :fecFin").setParameter("fecFin", this.fechaFin);
+		}
+		final String query = queryUtil.build();
 		String encoded = DatatypeConverter.printBase64Binary(query.getBytes());
 		request.getDatos().put(AppConstantes.QUERY, encoded);
 
 		return request;
 	}
+
 	public DatosRequest obtenerDelegaciones(DatosRequest request) {
 
-        SelectQueryUtil queryUtil = new SelectQueryUtil();
+		SelectQueryUtil queryUtil = new SelectQueryUtil();
 		queryUtil.select("sd.ID_DELEGACION, sd.DES_DELEGACION").from("svc_delegacion sd");
-        final String query = queryUtil.build();
+		final String query = queryUtil.build();
 		String encoded = DatatypeConverter.printBase64Binary(query.getBytes());
 		request.getDatos().put(AppConstantes.QUERY, encoded);
 		return request;
@@ -115,9 +116,9 @@ public class ConsultaDonado {
 
 	public DatosRequest obtenerVelatorio(DatosRequest request) {
 
-        SelectQueryUtil queryUtil = new SelectQueryUtil();
-        queryUtil.select("sv.ID_VELATORIO","sv.NOM_VELATORIO").from("svc_velatorio sv");
-        final String query = queryUtil.build();
+		SelectQueryUtil queryUtil = new SelectQueryUtil();
+		queryUtil.select("sv.ID_VELATORIO", "sv.NOM_VELATORIO").from("svc_velatorio sv");
+		final String query = queryUtil.build();
 		String encoded = DatatypeConverter.printBase64Binary(query.getBytes());
 		request.getDatos().put(AppConstantes.QUERY, encoded);
 		return request;
@@ -125,12 +126,89 @@ public class ConsultaDonado {
 
 	public DatosRequest obtenerNivel(DatosRequest request) {
 
-        SelectQueryUtil queryUtil = new SelectQueryUtil();
-        queryUtil.select("sno.ID_OFICINA","sno.DES_NIVELOFICINA")
-        .from("svc_nivel_oficina sno ");
-        final String query = queryUtil.build();
+		SelectQueryUtil queryUtil = new SelectQueryUtil();
+		queryUtil.select("sno.ID_OFICINA", "sno.DES_NIVELOFICINA").from("svc_nivel_oficina sno ");
+		final String query = queryUtil.build();
 		String encoded = DatatypeConverter.printBase64Binary(query.getBytes());
 		request.getDatos().put(AppConstantes.QUERY, encoded);
+		return request;
+	}
+
+	public DatosRequest consultarFiltroDonadosEntrada(DatosRequest request, String formatoFecha) {
+		SelectQueryUtil queryUtil = new SelectQueryUtil();
+		queryUtil
+				.select("si.ID_INVENTARIO AS idInventario", "sv.NOM_VELATORIO AS velatorio",
+						"stm.DES_TIPO_MATERIAL AS tipo", "sa.DES_MODELO_ARTICULO AS modeloAtaud",
+						"si.NUM_INVENTARIO AS numInventario",
+						"date_format(sd.FEC_ALTA ,'" + formatoFecha + "')  AS fecDonacion", "'Instituto' AS donadoPor",
+						"sp.NOM_PERSONA AS nomDonador")
+				.from("svc_donacion sd")
+				.join("svc_donacion_orden_servicio sdos", "sdos.ID_ORDEN_SERVICIO = sd.ID_ORDEN_SERVICIO")
+				.join(" svc_orden_servicio sos ", " sos.ID_ORDEN_SERVICIO = sdos.ID_ORDEN_SERVICIO")
+				.join(" svc_contratante sc ", " sc.ID_CONTRATANTE = sos.ID_CONTRATANTE")
+				.join(" svc_persona sp ", " sp.ID_PERSONA =sc.ID_PERSONA ")
+				.join(" svt_articulo sa ", " sa.ID_ARTICULO = sdos.ID_ARTICULO")
+				.join(" svc_tipo_articulo sta ", " sta.ID_TIPO_ARTICULO = sa.ID_TIPO_ARTICULO ")
+				.join(" svc_tipo_material stm ", " stm.ID_TIPO_MATERIAL = sa.ID_TIPO_MATERIAL")
+				.join(" svc_inventario si ", " si.ID_ARTICULO = sa.ID_ARTICULO ")
+				.join(" svc_velatorio sv ", " sv.ID_VELATORIO = si.ID_VELATORIO").where("sv.ID_VELATORIO = :idVel")
+				.setParameter("idVel", this.idVelatorio).and("sv.ID_DELEGACION = :idDel")
+				.setParameter("idDel", this.idDelegacion);
+		if (this.fechaInicio != null && this.fechaFin != null) {
+			queryUtil.and("date_format(sd.FEC_ALTA,'" + formatoFecha + "') >= :fecIni")
+					.setParameter("fecIni", this.fechaInicio)
+					.and("date_format(sd.FEC_ALTA ,'" + formatoFecha + "') <= :fecFin")
+					.setParameter("fecFin", this.fechaFin);
+		}
+		final String query = queryUtil.build();
+		String encoded = DatatypeConverter.printBase64Binary(query.getBytes());
+		request.getDatos().put(AppConstantes.QUERY, encoded);
+
+		return request;
+	}
+
+	public DatosRequest consultarDonados(DatosRequest request, String formatoFecha) {
+		String query = "SELECT si.ID_INVENTARIO AS idInventario,  sv.NOM_VELATORIO AS velatorio, stm.DES_TIPO_MATERIAL AS tipo "
+				+ ",  sa.DES_MODELO_ARTICULO AS modeloAtaud ,  si.NUM_INVENTARIO AS numInventario "
+				+ ",  date_format(ssd.FEC_SOLICITUD,'" + formatoFecha + "')  AS fecDonacion,  'ODS' AS donadoPor "
+				+ ",  sp.NOM_PERSONA AS nomDonador " + "FROM svc_inventario si  "
+				+ "JOIN svt_articulo sa ON sa.ID_ARTICULO = si.ID_ARTICULO  "
+				+ "JOIN svc_velatorio sv ON sv.ID_VELATORIO = si.ID_VELATORIO  "
+				+ "JOIN svc_tipo_material stm ON stm.ID_TIPO_MATERIAL = sa.ID_TIPO_MATERIAL  "
+				+ "JOIN svc_salida_donacion_ataudes ssda ON ssda.ID_ARTICULO = sa.ID_ARTICULO  "
+				+ "JOIN svc_salida_donacion ssd ON ssd.ID_SALIDA_DONACION  = ssda.ID_SALIDA_DONACION  "
+				+ "JOIN svc_contratante sc ON sc.ID_CONTRATANTE = ssd.ID_CONTRATANTE  "
+				+ "JOIN svc_persona sp ON sp.ID_PERSONA = sc.ID_PERSONA "
+				+ "JOIN svc_delegacion sd ON sd.ID_DELEGACION = sv.ID_DELEGACION  ";
+		if(this.idVelatorio != null && this.idDelegacion != null) 
+			query = query + "WHERE si.ID_VELATORIO = " + this.idVelatorio + "  AND sv.ID_DELEGACION = " + this.idDelegacion;
+		if (this.fechaInicio != null && this.fechaFin != null) {
+			query = query + " AND date_format(ssd.FEC_SOLICITUD,'%Y-%m-%d') >= '" + this.fechaInicio + "'"
+					+ " AND date_format(ssd.FEC_SOLICITUD,'%Y-%m-%d') <= '" + this.fechaFin + "'";
+		}
+		query = query + " UNION "
+				+ "SELECT si.ID_INVENTARIO AS idInventario,  sv.NOM_VELATORIO AS velatorio, stm.DES_TIPO_MATERIAL AS tipo "
+				+ ",  sa.DES_MODELO_ARTICULO AS modeloAtaud ,  si.NUM_INVENTARIO AS numInventario "
+				+ ",  date_format(sd.FEC_ALTA ,'%d/%m/%Y')  AS fecDonacion " + ",  'Instituto' AS donadoPor "
+				+ ",  sp.NOM_PERSONA AS nomDonador " + " FROM svc_donacion sd "
+				+ " JOIN svc_donacion_orden_servicio sdos ON sdos.ID_ORDEN_SERVICIO = sd.ID_ORDEN_SERVICIO "
+				+ " JOIN svc_orden_servicio sos ON sos.ID_ORDEN_SERVICIO = sdos.ID_ORDEN_SERVICIO "
+				+ " JOIN svc_contratante sc ON sc.ID_CONTRATANTE = sos.ID_CONTRATANTE "
+				+ " JOIN svc_persona sp ON sp.ID_PERSONA =sc.ID_PERSONA "
+				+ " JOIN svt_articulo sa ON sa.ID_ARTICULO = sdos.ID_ARTICULO "
+				+ " JOIN svc_tipo_articulo sta ON sta.ID_TIPO_ARTICULO = sa.ID_TIPO_ARTICULO "
+				+ " JOIN svc_tipo_material stm ON stm.ID_TIPO_MATERIAL = sa.ID_TIPO_MATERIAL "
+				+ " JOIN svc_inventario si ON si.ID_ARTICULO = sa.ID_ARTICULO "
+				+ " JOIN svc_velatorio sv ON sv.ID_VELATORIO = si.ID_VELATORIO ";
+		if(this.idVelatorio != null && this.idDelegacion != null) 
+				query = query +  " WHERE sv.ID_VELATORIO = " + this.idVelatorio + "  AND sv.ID_DELEGACION = " + this.idDelegacion;
+		if (this.fechaInicio != null && this.fechaFin != null) {
+			query = query + " AND date_format(sd.FEC_ALTA ,'%Y-%m-%d') >= '" + this.fechaInicio + "'"
+					+ " AND date_format(sd.FEC_ALTA ,'%Y-%m-%d') <= '" + this.fechaFin + "'";
+		}
+		String encoded = DatatypeConverter.printBase64Binary(query.getBytes());
+		request.getDatos().put(AppConstantes.QUERY, encoded);
+
 		return request;
 	}
 }
--- src/main/java/com/imss/sivimss/donaciones/service/impl/ConsultaDonadoServiceImpl.java
+++ src/main/java/com/imss/sivimss/donaciones/service/impl/ConsultaDonadoServiceImpl.java
@@ -1,9 +1,6 @@
 package com.imss.sivimss.donaciones.service.impl;
 
 import java.io.IOException;
-import java.util.*;
-
-import org.modelmapper.ModelMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,10 +10,8 @@ import org.springframework.stereotype.Service;
 import com.google.gson.Gson;
 import com.imss.sivimss.donaciones.beans.ConsultaDonado;
 import com.imss.sivimss.donaciones.model.request.ConsultaDonadoRequest;
-import com.imss.sivimss.donaciones.model.response.ConsultaDonadoDetalleResponse;
 import com.imss.sivimss.donaciones.service.ConsultaDonadosService;
 import com.imss.sivimss.donaciones.util.AppConstantes;
-import com.imss.sivimss.donaciones.util.ConvertirGenerico;
 import com.imss.sivimss.donaciones.util.DatosRequest;
 import com.imss.sivimss.donaciones.util.MensajeResponseUtil;
 import com.imss.sivimss.donaciones.util.ProviderServiceRestTemplate;
@@ -40,8 +35,6 @@ public class ConsultaDonadoServiceImpl implements ConsultaDonadosService {
 
 	ConsultaDonado consultarDonado = new ConsultaDonado();
 
-	@Autowired
-	private ModelMapper modelMapper;
 
 	private static final String NO_SE_ENCONTRO_INFORMACION = "45"; // No se encontró información relacionada a tu
 																	// búsqueda.
@@ -51,7 +44,7 @@ public class ConsultaDonadoServiceImpl implements ConsultaDonadosService {
 			throws IOException {
 
 		return MensajeResponseUtil.mensajeConsultaResponse(
-				providerRestTemplate.consumirServicio(consultarDonado.consultaDonado(request, formatoFecha).getDatos(),
+				providerRestTemplate.consumirServicio(consultarDonado.consultarDonados(request, formatoFecha).getDatos(),
 						urlConsultaGenericoPaginado, authentication),
 				NO_SE_ENCONTRO_INFORMACION);
 	}
@@ -65,12 +58,19 @@ public class ConsultaDonadoServiceImpl implements ConsultaDonadosService {
 		String datosJson = String.valueOf(request.getDatos().get(AppConstantes.DATOS));
 		ConsultaDonadoRequest consultaDonadoRequest = gson.fromJson(datosJson, ConsultaDonadoRequest.class);
 		consultarDonado = new ConsultaDonado(consultaDonadoRequest);
-		List<ConsultaDonadoDetalleResponse> permisoResponse;
+		Response<?> response = null ;
+		if(consultaDonadoRequest.getDonadoPor() == null) {
+			response = providerRestTemplate.consumirServicio(consultarDonado.consultarDonados(request, formatoFecha).getDatos(),
+					urlConsultaGenericoPaginado, authentication);
+		}else if(consultaDonadoRequest.getDonadoPor().equals("1") ){
+			response = providerRestTemplate.consumirServicio(consultarDonado.consultarFiltroDonadosSalida(request, formatoFecha).getDatos(),
+					urlConsultaGenericoPaginado, authentication);
+		}else if(consultaDonadoRequest.getDonadoPor().equals("2") ){
+				response = providerRestTemplate.consumirServicio(consultarDonado.consultarFiltroDonadosEntrada(request, formatoFecha).getDatos(),
+						urlConsultaGenericoPaginado, authentication);
+		}
 
-		return MensajeResponseUtil.mensajeConsultaResponse(
-				providerRestTemplate.consumirServicio(consultarDonado.consultarFiltroDonados(request, formatoFecha).getDatos(),
-						urlConsultaGenericoPaginado, authentication),
-				NO_SE_ENCONTRO_INFORMACION);
+		return MensajeResponseUtil.mensajeConsultaResponse( response, NO_SE_ENCONTRO_INFORMACION);
 	}
 	
 
